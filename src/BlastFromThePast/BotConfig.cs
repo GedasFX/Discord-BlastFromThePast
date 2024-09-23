a7c4588f7587c76815e8bfd5309e5b11da83f4b7
Gediminas <snappi@gedas.dev>
2024-09-23 23:15:32 +0300

    use env variables for bot

--- src/BlastFromThePast/BotConfig.cs
+++ src/BlastFromThePast/BotConfig.cs
@@ -1,21 +1,26 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-
-namespace BlastFromThePast;
+﻿namespace BlastFromThePast;
 
 public class BotConfig
 {
-    public static BotConfig Instance { get; } = GetConfig("config.json");
+    public static BotConfig Instance { get; } = GetConfig();
 
     public required string Token { get; set; }
     public ulong GuildId { get; set; }
 
-    public static BotConfig GetConfig(string path)
+    public static BotConfig GetConfig()
     {
-        using var stream = File.OpenRead(path);
-        return JsonSerializer.Deserialize(stream, SourceGenerationContext.Default.BotConfig)!;
+        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN") ??
+                    throw new Exception("Missing DISCORD_TOKEN environment variable");
+
+        var guildId = Environment.GetEnvironmentVariable("DISCORD_GUILD_ID") ??
+                      throw new Exception("Missing DISCORD_GUILD_ID environment variable");
+
+        return new BotConfig()
+        {
+            Token = token,
+            GuildId = ulong.TryParse(guildId, out var v) && v > 0
+                ? v
+                : throw new Exception("DISCORD_GUILD_ID is missing a value")
+        };
     }
 }
-
-[JsonSerializable(typeof(BotConfig))]
-internal partial class SourceGenerationContext : JsonSerializerContext;
